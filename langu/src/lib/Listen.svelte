62f180a45bee9b892017c9c5896ea218ef7c4ae6
Andriamanitra <mikko77792@gmail.com>
2022-07-15 08:50:35 +0300

    hide listening button if no speech synthesis available

--- langu/src/lib/Listen.svelte
+++ langu/src/lib/Listen.svelte
@@ -2,28 +2,36 @@
     import ButtonDropdown from "./ButtonDropdown.svelte";
     import DropdownItem from "./DropdownItem.svelte";
 
+    export let lang;
+    export let whatToSay;
+    export let buttonLabel = "Listen";
+    let voices;
+    let voice;
+
     const synth = window.speechSynthesis;
     const speak = () => {
         let utterance = new SpeechSynthesisUtterance(whatToSay);
         utterance.voice = voice;
         synth.speak(utterance);
     };
-    export let lang;
-    export let whatToSay;
-    export let buttonLabel = "Listen";
-    let voices = synth.getVoices().filter((v) => v.lang == lang);
-    let voice;
+    const getVoices = () => {
+        voices = synth.getVoices().filter((v) => v.lang == lang);
+    };
+    getVoices();
+    synth.addEventListener("voiceschanged", getVoices);
 </script>
 
-<ButtonDropdown onClick={speak} {buttonLabel}>
-    {#each voices as v, i}
-        <DropdownItem
-            onClick={() => {
-                voice = voices[i];
-                speak();
-            }}
-        >
-            {v.name.split("+")[1]}
-        </DropdownItem>
-    {/each}
-</ButtonDropdown>
+{#if voices.length > 0}
+    <ButtonDropdown onClick={speak} {buttonLabel}>
+        {#each voices as v, i}
+            <DropdownItem
+                onClick={() => {
+                    voice = voices[i];
+                    speak();
+                }}
+            >
+                {v.name.split("+")[1]}
+            </DropdownItem>
+        {/each}
+    </ButtonDropdown>
+{/if}
